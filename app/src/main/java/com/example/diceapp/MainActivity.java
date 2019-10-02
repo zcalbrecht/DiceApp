11ce285531032298f5f08926fe16557aea959720
zcalbrecht <zcalbrecht@gmail.com>
2019-10-01 21:08:51 -0500

    score

--- app/src/main/java/com/example/diceapp/MainActivity.java
+++ app/src/main/java/com/example/diceapp/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.diceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.Random;
 
 
@@ -31,7 +35,20 @@ public class MainActivity extends AppCompatActivity {
                 rollDice();
             }
         });
+
+        FloatingActionButton fab = findViewById(R.id.new_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go someplace new
+                Intent intent = new Intent(MainActivity.this, StatsActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
+
+
 
     private void rollDice() {
         int rand = rng.nextInt(20) + 1;
---
+++ app/src/main/java/com/example/diceapp/StatsActivity.java
@@ -0,0 +1,5 @@
+package com.example.diceapp;
+
+public class StatsActivity {
+
+}
